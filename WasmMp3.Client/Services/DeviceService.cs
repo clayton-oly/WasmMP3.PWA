220ada3d7807e2e7049a0ca2edf66e7599b2e492
Clayton Oliveira <clayton.will08@gmail.com>
2026-02-12 22:12:10 -0300

    Adiciona suporte a copiar/colar e nível de bateria nas notas
    Inclui botões para copiar e colar texto usando o clipboard do navegador e exibir o nível da bateria no componente de notas. Implementa métodos no DeviceService e integra funções JavaScript para clipboard e status da bateria, melhorando a experiência do usuário.

--- WasmMp3.Client/Services/DeviceService.cs
+++ WasmMp3.Client/Services/DeviceService.cs
@@ -15,4 +15,15 @@ public class DeviceService
 
     public ValueTask RegisterOnlineListenerAsync(IJSObjectReference dotNetObjRef)
         => _js.InvokeVoidAsync("device.onOnline", dotNetObjRef);
+
+    //metodos do CopyText (Clipboard)
+    public ValueTask<bool> CopyTextAsync(string txt)
+        => _js.InvokeAsync<bool>("clipboard.copyText", txt);
+
+    public ValueTask<string> ReadTextAsync()
+        => _js.InvokeAsync<string>("clipboard.readText");
+
+    public ValueTask<int> getLevel()
+    => _js.InvokeAsync<int>("battery.getLevel");
+
 }
